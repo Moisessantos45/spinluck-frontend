ac71d77ae4ed2502bb383f2feb59a08567cf7c63
Moisessantos45 <santosxphdz34@gmail.com>
2026-05-02 23:37:12 -0600

    feat: Add 404 Not Found page with user-friendly message and navigation

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-full h-screen flex flex-col items-center justify-center">
+        <h1 class="text-2xl font-bold">404 - Not Found</h1>
+        <p class="text-gray-600">The page you are looking for does not exist.</p>
+        <RouterLink to="/" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+            Go Home
+        </RouterLink>
+    </div>
+</template>
